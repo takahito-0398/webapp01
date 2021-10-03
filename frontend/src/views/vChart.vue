19eaf7715415b3ff53725943a86f7d00e96a2cd1
takahito-0398 <takahito_0398@yahoo.co.jp>
2021-10-03 23:38:40 +0900

    commit

---
+++ frontend/src/views/vChart.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="graph">
+    <h1>Bar & Line</h1>
+    <chart></chart>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Chart from '@/components/Chart.vue'
+
+export default {
+  name: 'graph',
+  components: {
+    Chart,
+  }
+}
+</script>
