99635174e3e164da6b81e15057383609763d4980
takahito-0398 <takahito_0398@yahoo.co.jp>
2021-10-03 21:53:58 +0900

    first commit

---
+++ run.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+app = Flask(__name__,
+            template_folder="./frontend/dist",
+            static_folder="./frontend/dist/static"
+            )
+
+name = "takahito"
+age = 24
+
+
+@app.route('/')
+def index():
+    # return render_template('index.html')
+    return render_template('index.html', name=name, age=age)
+
+
+@app.route('/about')
+def about():
+    return render_template('index.html')
+
+
+@app.route('/chart')
+def chart():
+    # return render_template('index.html')
+    return render_template('index.html', name=name, age=age)
+
+
+if __name__ == '__main__':
+    app.run()
